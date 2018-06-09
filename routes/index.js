490898a7e186d30bf7075bbcf355a6d41aeb2619
funkdog <32904860+funkdog@users.noreply.github.com>
2018-06-09 21:54:49 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ function initRoutes(app){
   var bodyParser = require('body-parser');
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(function(req,res,next){
-    console.log('222');
     res.sendFile = function(file){
         res.setHeader('X-Accel-Redirect','/protected/' + file);
         res.setHeader('Cache-Control','no-store');
@@ -29,7 +28,6 @@ function initRoutes(app){
     next();
 });
   app.get('/',function(req,res){
-    console.log('333');
     res.redirect('/homePage');
   });
   require('./page')(app);
