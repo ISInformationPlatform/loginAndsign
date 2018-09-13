afd776efbe5e9e8e06f3d6cea87cf682207f5448
funkdog <876311484@qq.com>
2018-09-13 23:03:44 +0800

    modified:   doc/api_project.js 	modified:   doc/api_project.json

--- doc/api_project.js
+++ doc/api_project.js
@@ -1,15 +1,16 @@
 define({
   "title": "用户管理模块",
+  "version": "0.0.1",
+  "descirption": "the apidoc of admin",
   "url": "http://localhost",
   "name": "admin",
-  "version": "0.0.1",
   "description": "用于用户管理的模块",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-09-04T06:02:33.534Z",
+    "time": "2018-09-08T08:23:06.839Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
